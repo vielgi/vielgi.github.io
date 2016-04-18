6488b83b47f9eaa43427f58c72ad1bdbfc95b909
vielgi <vgeorgiev@fas.harvard.edu>
2016-04-18 12:01:02 -0400

    Prototype 1 -- working version
    - line chart
    - bar chart

---
+++ js/bars.js
@@ -0,0 +1,377 @@
+var dataset;
+
+//Define bar chart function 
+function barChart(dataset) {
+
+    //Set width and height as fixed variables
+    var w = 800;
+    var h = 800;
+    var padding = 5;
+
+    //Scale function for axes and radius
+    var yScale = d3.scale.linear()
+                    .domain(d3.extent(dataset, function (d) { return d.gdp_change; }))
+                    .range([w + padding, padding]);
+
+    var xScale = d3.scale.ordinal()
+                    .domain(dataset.map(function (d) { return d.term; }))
+                    .rangeRoundBands([padding, h + padding], .5);
+
+    //To format axis as a percent
+    var formatPercent = d3.format("%1");
+
+    //Create y axis
+    var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5).tickFormat(formatPercent);
+
+    //Define key function
+    var key = function (d) { return d.term };
+
+    //Define tooltip for hover-over info windows
+    var div = d3.select("body").append("div")
+                        .attr("class", "tooltip")
+                        .style("opacity", 0);
+
+    //Create svg element
+    var svg = d3.select("#bars-area").append("svg")
+            .attr("width", w).attr("height", h)
+            .attr("id", "chart")
+            .attr("viewBox", "0 0 " + w + " " + h)
+            .attr("preserveAspectRatio", "xMinYMin");
+
+    //Resizing function to maintain aspect ratio (uses jquery)
+    var aspect = w / h;
+    var chart = $("#bars-area");
+    $(window).on("resize", function () {
+        var targetWidth = $("body").width();
+
+        if (targetWidth < w) {
+            chart.attr("width", targetWidth);
+            chart.attr("height", targetWidth / aspect);
+        }
+        else {
+            chart.attr("width", w);
+            chart.attr("height", w / aspect);
+        }
+
+    });
+
+
+    //Initialize state of chart according to drop down menu
+    var state = d3.selectAll("option");
+
+    //Create barchart
+    svg.selectAll("rect")
+        .data(dataset, key)
+        .enter()
+        .append("rect")
+        .attr("class", function (d) { return d.president_party == "R" ? "negative" : "positive"; })
+        .attr({
+            x: function (d) {
+                return xScale(d.term);
+            },
+            y: function (d) {
+                return yScale(Math.max(0, d.gdp_change));
+            },
+            width: xScale.rangeBand(),
+            height: function (d) {
+                return Math.abs(yScale(d.gdp_change) - yScale(0));
+            }
+        })
+        .on('mouseover', function (d) {
+            d3.select(this)
+                .style("opacity", 0.2)
+                .style("stroke", "black")
+
+            var info = div
+                        .style("opacity", 1)
+                        .style("left", (d3.event.pageX + 10) + "px")
+                        .style("top", (d3.event.pageY - 30) + "px")
+                        .text(d.term);
+
+            if (state[0][0].selected) {
+                info.append("p")
+                        .text(formatPercent(d.income_change) + " Median Income Change in %");
+
+            }
+            else if (state[0][1].selected) {
+                info.append("p")
+                        .text(formatPercent(d.gdp_change) + " GDP Change in %");
+            }
+            else if (state[0][2].selected) {
+                info.append("p")
+                        .text(formatPercent(d.cpi_change) + " CPI Change in %");
+            }
+            else if (state[0][3].selected) {
+                info.append("p")
+                        .text(formatPercent(d.cpi_change) + " S&P500 Change in %");
+            }
+            else if (state[0][4].selected) {
+                info.append("p")
+                        .text(formatPercent(d.cpi_change) + " Budget As % of GDP");
+            }
+            else if (state[0][5].selected) {
+                info.append("p")
+                        .text(formatPercent(d.cpi_change) + " Budget As % Change");
+            }
+            else if (state[0][6].selected) {
+                info.append("p")
+                        .text(formatPercent(d.cpi_change) + " Debt Change in %");
+            }
+            else if (state[0][7].selected) {
+                info.append("p")
+                        .text(formatPercent(d.cpi_change) + " Unemployment Change in %");
+            }
+            else if (state[0][8].selected) {
+                info.append("p")
+                        .text(formatPercent(d.cpi_change) + " Unemployment in %");
+            }
+
+
+
+        })
+                    .on('mouseout', function (d) {
+                        d3.select(this)
+                        .style({ 'stroke-opacity': 0.5, 'stroke': '#a8a8a8' })
+                        .style("opacity", 1);
+
+                        div
+                            .style("opacity", 0);
+                    });
+
+    //Add y-axis
+    svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(40,0)")
+            .call(yAxis);
+
+    //Sort data when sort is checked
+    d3.selectAll(".checkbox").
+    on("change", function () {
+        var x0 = xScale.domain(dataset.sort(sortChoice())
+        .map(function (d) { return d.term }))
+        .copy();
+
+        var transition = svg.transition().duration(750);
+        var delay = function (d, i) { return i * 10; };
+
+        transition.selectAll("rect")
+        .delay(delay)
+        .attr("x", function (d) { return x0(d.term); });
+
+    })
+
+    //Function to sort data when sort box is checked
+    function sortChoice() {
+        var state = d3.selectAll("option");
+        var sort = d3.selectAll(".checkbox");
+
+        if (sort[0][0].checked && state[0][0].selected) {
+            var out = function (a, b) { return b.gdp_change - a.gdp_change; }
+            return out;
+        }
+        else if (sort[0][0].checked && state[0][1].selected) {
+            var out = function (a, b) { return b.income_change - a.income_change; }
+            return out;
+        }
+        //from here   
+        else if (sort[0][0].checked && state[0][2].selected) {
+            var out = function (a, b) { return b.cpi_change - a.cpi_change; }
+            return out;
+        }
+
+        else if (sort[0][0].checked && state[0][3].selected) {
+            var out = function (a, b) { return b.sp_change - a.sp_change; }
+            return out;
+        }
+
+        else if (sort[0][0].checked && state[0][4].selected) {
+            var out = function (a, b) { return b.budget_change - a.budget_change; }
+            return out;
+        }
+
+        else if (sort[0][0].checked && state[0][5].selected) {
+            var out = function (a, b) { return b.budget_as_percentage - a.budget_as_percentage; }
+            return out;
+        }
+
+        else if (sort[0][0].checked && state[0][6].selected) {
+            var out = function (a, b) { return b.debt_change - a.debt_change; }
+            return out;
+        }
+
+        else if (sort[0][0].checked && state[0][7].selected) {
+            var out = function (a, b) { return b.unemployment_change - a.unemployment_change; }
+            return out;
+        }
+
+        else if (sort[0][0].checked && state[0][8].selected) {
+            var out = function (a, b) { return b.unemployment - a.unemployment; }
+            return out;
+        }
+
+        else {
+            var out = function (a, b) { return d3.ascending(a.term, b.term); }
+            return out;
+        }
+    };
+
+    //Change data to correct values on input change
+    d3.selectAll("select").
+    on("change", function () {
+
+        var value = this.value;
+
+        if (value == "gdp") {
+            var x_value = function (d) { return d.gdp_change; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.gdp_change));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.gdp_change) - yScale(0));
+            };
+        }
+
+        else if (value == "income") {
+            var x_value = function (d) { return d.income_change; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.income_change));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.income_change) - yScale(0));
+            };
+        }
+        
+        
+        else if (value == "sp") {
+            var x_value = function (d) { return d.sp_change; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.sp_change));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.sp_change) - yScale(0));
+            };
+        }
+
+        else if (value == "cpi") {
+            var x_value = function (d) { return d.cpi_change; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.cpi_change));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.cpi_change) - yScale(0));
+            };
+        }
+        else if (value == "budget") {
+            var x_value = function (d) { return d.budget_change; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.budget_change));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.budget_change) - yScale(0));
+            };
+        }
+
+        else if (value == "budgetpercentage") {
+            var x_value = function (d) { return d.budget_as_percentage; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.budget_as_percentage));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.budget_as_percentage) - yScale(0));
+            };
+        }
+        else if (value == "debt") {
+            var x_value = function (d) { return d.debt_change; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.debt_change));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.debt_change) - yScale(0));
+            };
+        }
+        else if (value == "unemployment_change") {
+            var x_value = function (d) { return d.unemployment_change; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.unemployment_change));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.unemployment_change) - yScale(0));
+            };
+        }
+        else if (value == "unemployment") {
+            var x_value = function (d) { return d.unemployment; };
+            var color = function (d) { return d.president_party == "R" ? "negative" : "positive"; };
+            var y_value = function (d) {
+                return yScale(Math.max(0, d.unemployment));
+            };
+            var height_value = function (d) {
+                return Math.abs(yScale(d.unemployment) - yScale(0));
+            };
+        }
+
+        //Update y scale
+        yScale.domain(d3.extent(dataset, x_value));
+
+        //Update with correct data
+        var rect = svg.selectAll("rect").data(dataset, key);
+        rect.exit().remove();
+
+        //Transition chart to new data
+        rect
+        .transition()
+        .duration(2000)
+        .ease("linear")
+        .each("start", function () {
+            d3.select(this)
+            .attr("width", "0.2")
+            .attr("class", color)
+        })
+        .attr({
+            x: function (d) {
+                return xScale(d.term);
+            },
+            y: y_value,
+            width: xScale.rangeBand(),
+            height: height_value
+
+        });
+
+        //Update y-axis
+        svg.select(".y.axis")
+            .transition()
+            .duration(1000)
+            .ease("linear")
+            .call(yAxis);
+    });
+
+};
+
+//Load data and call bar chart function 
+d3.csv("data/bars.csv", function (error, data) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        data.forEach(function (d) {
+            d.gdp_change = parseFloat(d.gdp_change);
+            d.income_change = parseFloat(d.income_change);
+            d.cpi_change = parseFloat(d.cpi_change);
+            d.sp_change = parseFloat(d.sp_change);
+            d.budget_change = parseFloat(d.budget_change);
+            d.budget_as_percentage = parseFloat(d.budget_as_percentage);
+            d.debt_change = parseFloat(d.debt_change);
+            d.unemployment_change = parseFloat(d.unemployment_change);
+            d.unemployment = parseFloat(d.unemployment);
+        });
+        dataset = data;
+        barChart(dataset);
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,349 @@
+//area of the line chart
+//var margin = {top: 20, right: 80, bottom: 30, left: 50},
+  //  width = 960 - margin.left - margin.right,
+    //height = 500 - margin.top - margin.bottom;
+
+var margin = {top: 20, right: 80, bottom: 120, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 520 - margin.top - margin.bottom;
+
+//upper and lower bound of my data
+var lb = "1947";
+var ub = "2013"
+
+//range slider
+$(function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 1947,
+      max: 2013,
+      values: [ 1947, 2013 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "From " + ui.values[ 0 ] + " - to " + ui.values[ 1 ] );
+          lb = String(ui.values[ 0 ])
+          ub = String(ui.values[ 1 ])
+          updateVisualization()
+      }
+    });
+    $( "#amount" ).val( "Year range: " + $( "#slider-range" ).slider( "values", 0 ) +
+      " - " + $( "#slider-range" ).slider( "values", 1 ) );
+  });
+
+// event listener to all checkboxes
+$(".myCheckBox").click(updateVisualization);
+
+//global variables to store viz data
+var globalData;
+var newEventData;
+var eventHeight = height + 25;
+
+// variable to get the value of the checked element
+var checkValue;
+
+//date parser
+var format = d3.time.format("%Y");
+
+//percentage parser
+var formatPercent = d3.format("1%")
+
+//x scale
+var x = d3.time.scale()
+    .range([0, width]);
+
+//y scale
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+//color scale for mulitple lines in the line chart
+var color = d3.scale.category10();
+
+//x axis
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+//y axis
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+//creating different indicator lines
+var gdpLine = d3.svg.line()
+    .interpolate("basis")
+   // .x(function(d) { return x(d.term); })
+    .y(function(d) { return y(d.gdp_change); });
+
+var budgetLine = d3.svg.line()
+    .interpolate("basis")
+    //.x(function(d) { return x(d.term); })
+    .y(function(d) { return y(d.budget_change); });
+
+var cpiLine = d3.svg.line()
+    .interpolate("basis")
+    //.x(function(d) { return x(d.term); })
+    .y(function(d) { return y(d.cpi_change); });
+
+var incomeLine = d3.svg.line()
+    .interpolate("basis")
+   // .x(function(d) { return x(d.term); })
+    .y(function(d) { return y(d.income_change); });
+
+//svg area
+var svg = d3.select("#lineChart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//line paths
+var gdpLinePath = svg.append("path")
+.attr("class", "gdpLine");
+
+var cpiLinePath = svg.append("path")
+.attr("class", "cpiLine");
+
+var incomeLinePath = svg.append("path")
+.attr("class", "incomeLine");
+
+var budgetLinePath = svg.append("path")
+.attr("class", "budgetLine");
+
+
+//loading president data
+//president terms line chart
+    var eventData = [
+        {"name": "Truman", "startdate": "1947",  "enddate": "1952", "party":"D"},
+        {"name": "Eisenhower", "startdate": "1953",  "enddate": "1960", "party":"R"},
+        {"name": "Kennedy", "startdate": "1961",  "enddate": "1963", "party":"D"},
+        {"name": "Johnson", "startdate": "1963",  "enddate": "1968", "party":"D"},
+        {"name": "Nixon", "startdate": "1969",  "enddate": "1974", "party":"R"},
+        {"name": "Ford", "startdate": "1974",  "enddate": "1976", "party":"R"},
+        {"name": "Carter", "startdate": "1977",  "enddate": "1980", "party":"D"},
+        {"name": "Regan", "startdate": "1981",  "enddate": "1988", "party":"R"},
+        {"name": "H.W. Bush", "startdate": "1989",  "enddate": "1992", "party":"R"},
+        {"name": "Clinton", "startdate": "1993",  "enddate": "2000", "party":"D"},
+        {"name": "W. Bush", "startdate": "2001",  "enddate": "2008", "party":"R"},
+        {"name": "Obama", "startdate": "2009",  "enddate": "2013", "party":"D"}
+    ];
+    
+    eventData.forEach(function(d) {
+        d.startdate = format.parse(d.startdate);
+        d.enddate = format.parse(d.enddate);
+    });
+    
+    
+
+//loading the data
+d3.csv("data/bars.csv", function(error, data) {
+  if (error) throw error;
+    //console.log(data);
+    
+    // Convert string to 'date object' and numeric objects
+    data.forEach(function(d){
+        d.term = format.parse(d.term)
+        d.gdp_change = +d.gdp_change * 100;
+        d.budget_change = +d.budget_change;
+        d.cpi_change = +d.cpi_change * 100;
+        d.income_change = +d.income_change * 100;
+    });
+    //console.log(data);
+    
+    //giving the data to a global variable
+    globalData = data
+    
+    // x scale domain
+    //x.domain(d3.extent(data, function(d) { return d.term; }));
+    
+    //y scale domain
+    //y.domain(d3.extent(data, function(d) { return d.gdp; }));
+    y.domain([-20,20])
+    
+    // group for x axis
+    var xAxisGroup = svg.append("g")
+      .attr("class", "x axis x-axis")
+      .attr("transform", "translate(0," + height + ")");
+      //.call(xAxis);
+    
+    //group for y axis
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Percentage (%)");
+    
+    //call the viz
+    updateVisualization();
+  });
+
+//drawing the mulit-line chart viz
+function updateVisualization() {
+    //date periods
+    var newData = globalData.filter(function(d) { return (d.term >= format.parse(lb) && d.term <= format.parse(ub)) });
+    
+    //setting the domain for the x scale
+    x.domain(d3.extent(newData, function(d) { return d.term; }));
+    
+    //line coordinates updated
+    gdpLine.x(function(d) { return x(d.term); })
+    cpiLine.x(function(d) { return x(d.term); })
+    budgetLine.x(function(d) { return x(d.term); })
+    incomeLine.x(function(d) { return x(d.term); })
+    
+    
+    //updating individual lines
+    if ($("#gdpCB").is(":checked")){
+        svg.select(".gdpLine")
+            .datum(newData)
+            .transition()
+            .duration(800)
+            .style("visibility","visible")
+            .attr("d", gdpLine);
+    }else{
+        svg.select(".gdpLine").style("visibility","hidden");
+    }
+
+    if ($("#cpiCB").is(":checked")){
+        svg.select(".cpiLine")
+            .datum(newData)
+            .attr("d", cpiLine)
+            .style("visibility","visible");
+    }else{
+        svg.select(".cpiLine").style("visibility","hidden");
+    }
+    
+    if ($("#incomeCB").is(":checked")){
+        svg.select(".incomeLine")
+            .datum(newData)
+            .style("visibility","visible")
+            .attr("d", incomeLine);
+    }else{
+        svg.select(".incomeLine").style("visibility","hidden");
+    }
+
+    if ($("#budgetCB").is(":checked")){
+        svg.select(".budgetLine")
+            .datum(newData)
+            .style("visibility","visible")
+            .attr("d", budgetLine);
+    }else{
+        svg.select(".budgetLine").style("visibility","hidden");
+    }
+    
+    //updating the x axis
+    svg.select(".x-axis")
+        .transition()
+        .duration(800)
+        .call(xAxis);
+    
+    
+    newEventData = eventData.filter(function(d) { return (d.startdate >= format.parse(lb) && d.enddate <= format.parse(ub)) });
+    
+    //console.log(newEventData)
+    
+    var eventInd = svg.selectAll(".eventInd")
+    .data(newEventData)
+    var eventText = svg.selectAll(".eventtitle")
+    .data(newEventData)
+    var eventEnd = svg.selectAll(".eventEnd")
+    .data(newEventData)
+    var eventCon = svg.selectAll("eventlineconnector")
+    .data(newEventData)
+    //.enter()
+    //.append("g")
+    //.attr("class", "eventInd");
+    
+    // draw a line: start event
+    eventInd.enter().append("line")
+        .attr("class", "eventInd eventlineborder")
+        //.attr("x1", function(d) { return x(d.startdate); })
+        .attr("y1", 0)
+        //.attr("x2", function(d) { return x(d.startdate); })
+        .attr("y2", eventHeight)
+        //.attr("class","eventlineborder")
+        //.style("stroke",function(d){
+          //  if (d.party ==="D"){return "#66d9ff"}
+            //else{return "#ff6666"}
+        //});
+    eventInd
+        .attr("x1", function(d) { return x(d.startdate); })
+        //.attr("y1", 0)
+        .attr("x2", function(d) { return x(d.startdate); })
+        //.attr("y2", eventHeight)
+        .style("stroke",function(d){
+            if (d.party ==="D"){return "#66d9ff"}
+            else{return "#ff6666"}
+        });
+    
+  // draw a line: end event
+    eventEnd.enter().append("line")
+        .attr("class", "eventEnd eventlineborder")
+        //.attr("x1", function(d) { return x(d.enddate); })
+        .attr("y1", 0)
+        //.attr("x2", function(d) { return x(d.enddate); })
+        .attr("y2", eventHeight)
+        //.attr("class","eventlineborder")
+        //.style("stroke",function(d){
+          //  if (d.party ==="D"){return "#66d9ff"}
+            //else{return "#ff6666"}
+        //});
+    
+    eventEnd
+        .attr("x1", function(d) { return x(d.enddate); })
+        .attr("x2", function(d) { return x(d.enddate); })
+        .style("stroke",function(d){
+            if (d.party ==="D"){return "#66d9ff"}
+            else{return "#ff6666"}
+        });
+    // draw a line: period connector event
+   eventCon.enter().append("line")
+        .attr("class", "eventlineconnector")
+        //.attr("x1", function(d) { return x(d.startdate); })
+        .attr("y1", eventHeight)
+       // .attr("x2", function(d) { return x(d.enddate); })
+        .attr("y2", eventHeight)
+        //.attr("class","eventlineconnector")
+        //.style("stroke",function(d){
+          //  if (d.party ==="D"){return "#66d9ff"}
+            //else{return "#ff6666"}
+        //});
+    eventCon
+        .attr("x1", function(d) { return x(d.startdate); })
+        .attr("x2", function(d) { return x(d.enddate); })
+        .style("stroke",function(d){
+            if (d.party ==="D"){return "#66d9ff"}
+            else{return "#ff6666"}
+        });
+    // add event label
+    //eventInd.append("text")
+    eventText.enter().append("text")
+        .attr("class", "eventtitle")
+        .attr("x",eventHeight + 5)
+        .attr("transform", "rotate(90)")
+        //.attr("x", function(d) { return x(d.startdate) + ((x(d.enddate) - x(d.startdate))/2); })
+        //.attr("y", eventHeight + 12)
+       /* .attr("x",eventHeight + 5)
+        .attr("y",function(d) { return -1* (x(d.startdate) + ((x(d.enddate) - x(d.startdate))/2)); })
+        .attr("class", "eventtitle")
+        .text(function(d) { return d.name; })
+        .attr("transform", "rotate(90)")
+        .style("stroke",function(d){
+            if (d.party ==="D"){return "#66d9ff"}
+            else{return "#ff6666"}
+        });*/
+    eventText
+        .attr("y",function(d) { return -1* (x(d.startdate) + ((x(d.enddate) - x(d.startdate))/2)); })
+        .text(function(d) { return d.name; })
+        .style("stroke",function(d){
+            if (d.party ==="D"){return "#66d9ff"}
+            else{return "#ff6666"}
+        });
+    
+    eventInd.exit().remove();
+    eventEnd.exit().remove();
+    eventCon.exit().remove();
+    eventText.exit().remove();
+}
